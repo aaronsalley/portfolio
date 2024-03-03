5a6cc8fbb55de6a8dc9dd41b4f8c64c5cb4a820f
Aaron Salley <aaron@aaronsalley.com>
2024-03-02 19:39:12 -0500

    Fixed manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,30 +1,31 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Aaron Salley',
-    short_name: 'Portfolio',
-    description: 'Portfolio for Aaron Salley',
-    start_url: '/?source=pwa',
-    display: 'standalone',
+    name: "Aaron Salley",
+    short_name: "Portfolio",
+    description: "Portfolio for Aaron Salley",
+    start_url: "/",
+    id: "?source=pwa",
+    display: "standalone",
     icons: [
       {
-        src: '/memoji_computer.svg',
-        sizes: 'any',
+        src: "/memoji_computer.svg",
+        sizes: "any",
       },
     ],
-    theme_color: '#f8f9fa',
-    background_color: '#f8f9fa',
+    theme_color: "#f8f9fa",
+    background_color: "#f8f9fa",
     shortcuts: [
       {
-        name: 'Contact me',
-        short_name: 'Email',
-        description: 'Get in touch to work together',
-        url: '/contact',
+        name: "Contact me",
+        short_name: "Email",
+        description: "Get in touch to work together",
+        url: "/contact",
         icons: [
           {
-            src: '/media/Aaron-Salley-Disruptv-NYC-icon--onLight192.png',
-            sizes: '192x192',
+            src: "/memoji_computer.svg",
+            sizes: "420x420",
           },
         ],
       },
