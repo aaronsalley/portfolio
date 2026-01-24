ba33f43439f9d0f1b53e6111b03c97053d3a99d5
Aaron Salley <aaron@aaronsalley.com>
2026-01-24 09:22:22 -0500

    Blog placeholder images

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -4,17 +4,19 @@ import Link from 'next/link';
 
 export default function PostCard(post: Post) {
   return (
-    <li key={post.id} className="rounded-2xl bg-salley-light">
+    <li key={post.id} className="group rounded-2xl bg-salley-light">
       <Link href={post.url} target="_blank">
         <article>
-          {post.image && (
+          {post.image ? (
             <Image
               src={post.image}
               alt={post.title}
               width={800}
               height={400}
-              className="aspect-7/5 w-full rounded-2xl bg-black object-cover"
+              className="aspect-7/5 w-full rounded-2xl object-cover"
             />
+          ) : (
+            <div className="group:bg-[url('/placeholder2.jpg')] aspect-7/5 w-full rounded-2xl bg-cover bg-center group-first:bg-[url('/placeholder1.jpg')] group-last:bg-[url('/placeholder1.jpg')]"></div>
           )}
           <ul className="mt-5 ml-5 inline-flex">
             {post.tags.length > 0 && (
